d2f88e643dda8eb1881738d28cab57ff32d6d201
simba155korea <simba155korea@gmail.com>
2021-12-19 15:03:26 +0900

    1

--- stocksafe_vue_v1/src/main.js
+++ stocksafe_vue_v1/src/main.js
@@ -2,8 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false;
+
 
 new Vue({
   router,
