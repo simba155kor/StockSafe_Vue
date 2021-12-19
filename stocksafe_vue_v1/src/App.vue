e899d54aa10d8064def9e39baaa72942352a8605
hyuna919 <hyuna919@naver.com>
2021-12-19 16:40:14 +0900

    UPDATE separate NaviBar

--- stocksafe_vue_v1/src/App.vue
+++ stocksafe_vue_v1/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navi-bar />
     <router-view />
   </div>
 </template>
+
 <script>
-export default {};
+import NaviBar from "./components/layout/NaviBar.vue";
+export default {
+  components: {
+    NaviBar,
+  },
+};
 </script>
 
 <style>
