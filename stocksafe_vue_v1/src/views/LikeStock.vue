c08995b1ec228d1b451504842f81462c47eb04d7
hyuna919 <hyuna919@naver.com>
2021-12-22 17:25:57 +0900

    nothing

--- stocksafe_vue_v1/src/views/LikeStock.vue
+++ stocksafe_vue_v1/src/views/LikeStock.vue
@@ -1,10 +1,55 @@
 <template>
-  <div>likestock</div>
+  <!-- Page Wrapper -->
+  <div class="container-fluid">
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">관심 주식 관리</h1>
+    <p class="mb-4">
+      관심표시한 주식을 확인합니다. facebook의 시계열 예측모델 Prophet을 이용한
+      3일후의 예측 가격을 제공하나 참고용으로만 확인하십시오. <br />
+      거래의 책임은 거래 당사자에게 있습니다.
+      <a target="_blank" href="https://facebook.github.io/prophet/"
+        >official Prophet documentation</a
+      >.
+    </p>
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+      <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">관심 주식 목록</h6>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive">
+          <list-table :data="data" :columns="columns" type="myStock">
+          </list-table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import ListTable from "@/components/list/ListTable.vue";
+
+export default {
+  name: "MyStock",
+  data() {
+    return {
+      data: [],
+      // 예측 가격을 3일, 1주일, 1달 단위로 여러개 보여줘도 될것같지만 이건 서버 비용을 고려해봐야
+      columns: ["종목명", "예측가격"],
+    };
+  },
+  components: {
+    ListTable,
+  },
+  mounted() {
+    this.data = [
+      { no: 3, name: "카카오", price: "333" },
+      { no: 4, name: "네이버", price: "444" },
+      { no: 5, name: "쿠팡", price: "555" },
+    ];
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
