f77ccfb6ce16f78dd5ce65198def294129152cd9
hyuna919 <hyuna919@naver.com>
2022-08-25 14:37:39 +0900

    FEATURE  OAuth 데이터를 서버로 넘겨줌

---
+++ stocksafe_vue_v1/src/views/Oauth2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+import http from "@/utils/http-common.js";
+
+export default {
+  name: "Oauth2",
+  created() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+    http
+      .post(`/login/oauth/google`, {
+        code: code,
+      })
+      .then((res) => {
+        this.data = res.data;
+        // ^^서버에서 로그인 성공인지 아닌지 알려주라
+      });
+  },
+};
+</script>
+
+<style></style>
